6a59f60b657cf31dd1e2a0591a93fe19b15207d1
Shubham <shubhamgoel879@gmail.com>
2022-08-09 00:18:03 +0530

    [Improved scss of homepage and added async await for posts fetch]

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,29 +1,26 @@
 const Post = require('../models/post');
 const User = require('../models/user');
-module.exports.home = function (req, res) {
+module.exports.home = async function (req, res) {
 
     /* Post.find will find all possible post objects in an array, 
     each object having content as string and user as objectId 
     so we have to populate our user using ref collection='User', now user is now having name, email etc. */
 
-
-    Post.find({})
+    let posts = await Post.find({})
         .populate('user')
         .populate({
             path: 'comments',
             populate: {
                 path: 'user'
             }
-        })
-        .exec(function (err, posts) {
-            if (err) { console.log('Error in finding all posts'); return; }
-            User.find({}, function (err, users) {
-                return res.render('home', {
-                    title: 'Home',
-                    posts: posts,
-                    all_users:users
-                });
-            });
-
         });
+
+
+    let users = await User.find({});
+
+    return res.render('home', {
+        title: 'Home',
+        posts: posts,
+        all_users: users
+    });
 };
